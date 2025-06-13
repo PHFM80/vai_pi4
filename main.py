3bb531702150b6808f25908d43a87e5071d468f3
PHFM80 <pablofloresmaza@gmail.com>
2025-06-13 10:57:05 -0300

    iniciador de bd agregado a main

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import asyncio
 from fastapi import FastAPI
 from plc.collector_sensores import run as run_sensores
 from plc.collector_actuadores import run as run_actuadores
+from app.database import init_db  
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
+    print("[DEBUG] Inicializando base de datos...")
+    init_db()  # ← Esta línea crea las tablas si no existen
     print("[DEBUG] Lanzando collector_sensores...")
     asyncio.create_task(run_sensores())
     print("[DEBUG] Lanzando collector_actuadores...")
