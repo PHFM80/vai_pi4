8c648c80d241a61a7ad6941a0a88ccd8e30eb2fa
PHFM80 <pablofloresmaza@gmail.com>
2025-06-13 10:51:36 -0300

    modelo sensor corregido

--- models/sensores.py
+++ models/sensores.py
@@ -1,15 +1,12 @@
 #D:\vai_pi4\models\sensores.py
-from sqlalchemy import Column, Integer, String, Date, Time
+from sqlalchemy import Column, Integer, Float, Date, Time
 from app.database import Base  # importar Base desde database.py
 
-class EventoActuador(Base):
-    __tablename__ = 'evento_actuador'
+class SensorLectura(Base):
+    __tablename__ = 'sensor_lecturas'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    accion = Column(String(50), nullable=False)
+    id_sensor = Column(Integer, nullable=False)
+    valor = Column(Float, nullable=False)
     fecha = Column(Date, nullable=False)
     hora = Column(Time, nullable=False)
-    actuador = Column(Integer, nullable=False)
-    origen_evento = Column(String(20), nullable=False, default='plc')
-    usuario = Column(Integer, nullable=True)
-    controlador = Column(Integer, nullable=True)
