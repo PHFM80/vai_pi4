7eebada71a4e91962c75d434da61c3be3c655cf3
PHFM80 <pablofloresmaza@gmail.com>
2025-06-16 22:18:37 -0300

    prueba lectura estado Q1

--- plc/connection.py
+++ plc/connection.py
@@ -4,11 +4,11 @@ from snap7.logo import Logo
 class LOGOConnection:
     def __init__(self, ip: str):
         self.ip = ip
-        self.client = Logo()  # Crear instancia sin IP
+        self.client = Logo()
 
-    def conectar(self) -> None:
+    def conectar(self):
         try:
-            self.client.connect(self.ip, 0, 0)  # Conectar con IP, rack=0, slot=0
+            self.client.connect(self.ip)  # Solo IP, sin rack ni slot
             if self.client.get_connected():
                 print(f"Conectado a PLC en {self.ip}")
             else:
@@ -16,12 +16,11 @@ class LOGOConnection:
         except Exception as e:
             print(f"[ERROR] Fallo conexión al PLC {self.ip}: {e}")
 
-    def desconectar(self) -> None:
+    def desconectar(self):
         try:
             self.client.disconnect()
             print(f"Desconectado de PLC en {self.ip}")
         except Exception as e:
             print(f"[ERROR] Fallo al desconectar del PLC {self.ip}: {e}")
-
 
 
