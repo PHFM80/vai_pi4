8e9efa030d1be7a5317599647cfe9ed48574b357
PHFM80 <pablofloresmaza@gmail.com>
2025-06-18 11:44:35 -0300

    trabajo en envio de eventos actuadores desde la pi al servidor

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -2,24 +2,31 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 import atexit
 import logging
-from scheduler.envio_datos_sensores import def_envio_datos_sensores 
 
-# Crear scheduler de fondo
+from scheduler.envio_datos_sensores import def_envio_datos_sensores
+from scheduler.envio_eventos_actuadores import def_envio_eventos_actuadores  # NUEVO
+
 scheduler = BackgroundScheduler()
-
-# Configurar logging para errores
 logger = logging.getLogger(__name__)
 
 def iniciar_scheduler():
     minutes = 3
+
     scheduler.add_job(
         def_envio_datos_sensores,
         trigger=IntervalTrigger(minutes=minutes),
         id="envio_datos_sensores",
         replace_existing=True
     )
+
+    scheduler.add_job(  # NUEVO
+        def_envio_eventos_actuadores,
+        trigger=IntervalTrigger(minutes=minutes),
+        id="envio_eventos_actuadores",
+        replace_existing=True
+    )
+
     scheduler.start()
     print("[DEBUG] Scheduler iniciado")
 
-    # Detener el scheduler al cerrar la app
     atexit.register(lambda: scheduler.shutdown())
